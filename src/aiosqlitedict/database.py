1e5e39b570da068fea4dd4bd70cf2783a5fc250c
Abdelrahman Sabry <51752028+sabrysm@users.noreply.github.com>
2022-10-12 06:17:00 +0200

    v0.0.29

--- src/aiosqlitedict/database.py
+++ src/aiosqlitedict/database.py
@@ -1,18 +1,46 @@
-import aiosqlite
 from ast import literal_eval
+import aiosqlite
 
 
-class Connect:
+class SQLCommands(object):
+    def __init__(self, database_name: str, table_name: str, primary_key: str):
+        self._database_name = database_name
+        self._table_name = table_name
+        self._primary_key = primary_key
+
+    async def delete(self, row_id):
+        """
+        deletes a certain row from the table.
+        :param row_id: The id of the row.
+        :type row_id: int
+        """
+        async with aiosqlite.connect(self._database_name) as db:
+            async with db.cursor() as cursor:
+                await cursor.execute(
+                    f"DELETE FROM {self._table_name} WHERE {self._primary_key} = ?", (row_id,))
+            await db.commit()
+
+    async def execute(self, query):
+        """
+        Execute SQL query.
+        """
+        async with aiosqlite.connect(self._database_name) as db:
+            async with db.cursor() as cursor:
+                cur = await cursor.execute(query)
+                res = await cur.fetchall()
+            await db.commit()
+            return res
+
+
+class Connect(SQLCommands):
     """
     Instantiate a conversion to and from sqlite3 database and python dictionary.
     """
 
-    def __init__(self, database_name: str, table_name: str, id_column: str):
-        self.database_name = database_name
-        self.table_name = table_name
-        self.id_column = id_column
+    def __init__(self, database_name: str, table_name: str, primary_key: str):
+        super().__init__(database_name, table_name, primary_key)
 
-    async def to_dict(self, my_id, *column_names: str):
+    async def to_dict(self, my_id, *column_names: str) -> dict:
         """
         Convert a sqlite3 table into a python dictionary.
         :param my_id: The id of the row.
@@ -22,46 +50,49 @@ class Connect:
         :return: The dictionary.
         :rtype: dict
         """
-        async with aiosqlite.connect(self.database_name) as db:
+        async with aiosqlite.connect(self._database_name) as db:
             async with db.cursor() as cursor:
-
-                table_name = self.table_name.replace("'", "").replace('"', "")
                 data = {}
-                columns = str(column_names).replace("(", "").replace(
-                    ")", "").replace('"', "").replace("'", "")
-                columns = columns.replace(
-                    columns[-1], "") if columns.endswith(",") else columns
+                columns = ", ".join(column for column in column_names)
+
+                def check_type(value, value_type):
+                    # to check if value is of certain type
+                    try:
+                        val = str(value)
+                        return isinstance(literal_eval(val), value_type)
+                    except SyntaxError:
+                        return False
+                    except ValueError:
+                        return False
 
                 if columns == "*":
                     getID = await cursor.execute(
-                        f"SELECT {columns} FROM {table_name} WHERE {self.id_column} = ?", (my_id,))
+                        f"SELECT {columns} FROM {self._table_name} WHERE {self._primary_key} = ?", (my_id,))
                     fieldnames = [f[0] for f in getID.description]
                     values = await getID.fetchone()
                     values = list(values)
                     for v in range(len(values)):
-                        if str(values[v]).startswith("[") or str(values[v]).startswith("{") or str(values[v]).startswith("("):
-                            try:
+                        if not (isinstance(values[v], int) or isinstance(values[v], float)):
+                            isList = check_type(values[v], list)
+                            isTuple = check_type(values[v], tuple)
+                            isDict = check_type(values[v], dict)
+                            if isList or isTuple or isDict:
                                 values[v] = literal_eval(values[v])
-                            except Exception as e:
-                                raise Exception(e)
-                        else:
-                            continue
                     for i in range(len(fieldnames)):
                         data[fieldnames[i]] = values[i]
                     return data
                 else:
                     getID = await cursor.execute(
-                        f"SELECT {columns} FROM {table_name} WHERE {self.id_column} = ?", (my_id, ))
+                        f"SELECT {columns} FROM {self._table_name} WHERE {self._primary_key} = ?", (my_id,))
                     values = await getID.fetchone()
                     values = list(values)
                     for v in range(len(values)):
-                        if str(values[v]).startswith("[") or str(values[v]).startswith("{") or str(values[v]).startswith("("):
-                            try:
+                        if not (isinstance(values[v], int) or isinstance(values[v], float)):
+                            isList = check_type(values[v], list)
+                            isTuple = check_type(values[v], tuple)
+                            isDict = check_type(values[v], dict)
+                            if isList or isTuple or isDict:
                                 values[v] = literal_eval(values[v])
-                            except Exception as e:
-                                raise Exception(e)
-                        else:
-                            continue
                     for i in range(len(column_names)):
                         data[column_names[i]] = values[i]
                     return data
@@ -78,31 +109,33 @@ class Connect:
         :return: The SQLite3 Table.
         :rtype: sqlite
         """
-        async with aiosqlite.connect(self.database_name) as db:
+        async with aiosqlite.connect(self._database_name) as db:
             async with db.cursor() as cursor:
-                table_name = self.table_name
-                getUser = await cursor.execute(f"SELECT {self.id_column} FROM {table_name} WHERE {self.id_column} = ?", (my_id, ))
+                getUser = await cursor. \
+                    execute(f"SELECT {self._primary_key} FROM {self._table_name} WHERE {self._primary_key} = ?",
+                            (my_id,))
                 isUserExists = await getUser.fetchone()
                 if isUserExists:
                     for key, val in dictionary.items():
-                        if str(val).startswith("[") or str(val).startswith("{") or str(val).startswith("("):
-                            val = str(val)
-                        else:
-                            pass
-                        await cursor.execute(f"UPDATE {table_name} SET {key} = ? WHERE {self.id_column} = ?", (val, my_id,))
+                        if isinstance(val, list) or isinstance(val, dict) or isinstance(val, tuple):
+                            dictionary[key] = str(val)
+                    await cursor.execute(f"UPDATE {self._table_name} SET " + ', '.join(
+                        "{}=?".format(k) for k in dictionary.keys()) + f" WHERE {self._primary_key}=?",
+                                         list(dictionary.values()) + [my_id])
                 else:
-                    await cursor.execute(f"INSERT INTO {table_name} ({self.id_column}) VALUES ( ? )", (my_id, ))
+                    await cursor.execute(f"INSERT INTO {self._table_name} ({self._primary_key}) VALUES ( ? )", (my_id,))
                     for key, val in dictionary.items():
-                        if str(val).startswith("[") or str(val).startswith("{") or str(val).startswith("("):
-                            val = str(val)
-                        else:
-                            pass
-                        await cursor.execute(f"UPDATE {table_name} SET {key} = ? WHERE {self.id_column} = ?", (val, my_id,))
+                        if isinstance(val, list) or isinstance(val, dict) or isinstance(val, tuple):
+                            dictionary[key] = str(val)
+                    await cursor.execute(f"UPDATE {self._table_name} SET " + ', '.join(
+                        "{}=?".format(k) for k in dictionary.keys()) + f" WHERE {self._primary_key}=?",
+                                         list(dictionary.values()) + [my_id])
 
             await db.commit()
 
-    async def select(self, column_name: str, limit: int = None, order_by: str = None, ascending: bool = True
-                     , equal=None, like: str = None, between: tuple = None, distinct: bool = False, offset: int = None):
+    async def select(self, column_name: str, limit: int = None, order_by: str = None,
+                     ascending: bool = True, equal=None, like: str = None, between: tuple = None,
+                     distinct: bool = False, offset: int = None) -> list:
         """
         Select a column from the table.
 
@@ -126,28 +159,28 @@ class Connect:
         :return: The list.
         :rtype: list
         """
-        async with aiosqlite.connect(self.database_name) as db:
+        async with aiosqlite.connect(self._database_name) as db:
             async with db.cursor() as cursor:
 
-                query = f"SELECT {column_name} FROM {self.table_name}"
+                query = f"SELECT {column_name} FROM {self._table_name}"
                 parameters = []
                 condition = False
                 if distinct is True:
-                    query = f"SELECT DISTINCT {column_name} FROM {self.table_name}"
+                    query = f"SELECT DISTINCT {column_name} FROM {self._table_name}"
                 if equal is not None and condition is False:
                     condition = True
                     query += f" WHERE {column_name} = ?"
-                    parameters.append(equal,)
+                    parameters.append(equal)
                 elif equal is not None and condition is True:
                     query += f" AND {column_name} = ?"
                     parameters.append(equal)
                 if like is not None and condition is False:
                     condition = True
                     query += f" WHERE {column_name} LIKE ?"
-                    parameters.append("%"+like+"%")
+                    parameters.append("%" + like + "%")
                 elif like is not None and condition is True:
                     query += f" AND {column_name} LIKE ?"
-                    parameters.append("%"+like+"%")
+                    parameters.append("%" + like + "%")
                 if between is not None and condition is False:
                     condition = True
                     query += f" WHERE {column_name} BETWEEN ? AND ?"
@@ -180,10 +213,13 @@ class Connect:
                 values = await getValues.fetchall()
                 my_list = []
 
-                def isfloat(num):
+                def check_type(value, value_type):
+                    # to check if value is of certain type
                     try:
-                        float(num)
-                        return True
+                        val = str(value)
+                        return isinstance(literal_eval(val), value_type)
+                    except SyntaxError:
+                        return False
                     except ValueError:
                         return False
 
@@ -193,11 +229,11 @@ class Connect:
                     # Check the type of i
                     if i.isnumeric():
                         my_list.append(int(i))
-                    elif isfloat(i):
+                    elif isinstance(i, float):
                         my_list.append(float(i))
                     elif i == 'None' or i is None:
                         my_list.append(i)
-                    elif i.startswith("'[") or i.startswith("'{") or i.startswith("'("):
+                    elif check_type(i, list) or check_type(i, dict) or check_type(i, tuple):
                         i = eval(i)
                         my_list.append(eval(i))
                     elif i.isascii():
@@ -207,27 +243,3 @@ class Connect:
                         my_list.append(i)
 
                 return my_list
-
-    async def delete(self, row_id):
-        """
-        deletes a certain row from the table.
-        :param row_id: The id of the row.
-        :type row_id: int
-        """
-        async with aiosqlite.connect(self.database_name) as db:
-            async with db.cursor() as cursor:
-                await cursor.execute(
-                    f"DELETE FROM {self.table_name} WHERE {self.id_column} = ?", (row_id, ))
-            await db.commit()
-
-    async def execute(self, query):
-        """
-        Execute SQL query.
-        """
-        async with aiosqlite.connect(self.database_name) as db:
-            async with db.cursor() as cursor:
-                cur = await cursor.execute(query)
-                res = await cur.fetchall()
-            await db.commit()
-            return res
-
